4ee4f146eab63c0e42515ea5dba3a2f822c0ab9a
developer-white-falcon <111103876+developer-white-falcon@users.noreply.github.com>
2024-11-16 22:21:11 +0300

    Delete js directory

--- js/menu-hamburger.js
+++
@@ -1,9 +0,0 @@
-let link = document.getElementById('link');
-let burger = document.getElementById('burger');
-let ul = document.querySelector('.list-unstyled ');
-
-link.addEventListener('click', function (e) {
-  e.preventDefault();
-  burger.classList.toggle('open');
-  ul.classList.toggle('hamburger');
-});
