9292635889c51e8598af3f3ad1ec3b3004f5f829
developer-white-falcon <111103876+developer-white-falcon@users.noreply.github.com>
2024-11-20 00:33:57 +0300

    dsd5

---
+++ script.js
@@ -0,0 +1,56 @@
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+// Swiper
+// Chart.js
+
+  const config = {
+    type: 'line',
+    data: data,
+  };
+
+//
+  const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+//  Chart.js
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
